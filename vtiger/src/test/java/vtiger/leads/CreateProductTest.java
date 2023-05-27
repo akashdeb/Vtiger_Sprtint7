5f7b9aa92b2ea12cb8a05c050af0c59653945ea1
akashdeb <akashdeb87@gmail.com>
2023-05-27 16:20:55 +0530

    Checking for Git Conflict

--- vtiger/src/test/java/vtiger/leads/CreateProductTest.java
+++ vtiger/src/test/java/vtiger/leads/CreateProductTest.java
@@ -12,5 +12,6 @@ public class CreateProductTest {
 	@Test
 	public void createProductAndVerifyProductWithVendorDetails() {
 		System.out.println("The Product has been created and vendor deatails has been verified");
+	
 	}
 }
