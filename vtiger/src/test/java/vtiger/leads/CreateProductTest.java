a4f01adbc1c0e29d390e6eb9135298178c1a4800
akashdeb <akashdeb87@gmail.com>
2023-05-27 16:03:57 +0530

    verified Product details

---
+++ vtiger/src/test/java/vtiger/leads/CreateProductTest.java
@@ -0,0 +1,11 @@
+package vtiger.leads;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+
+	@Test
+	public void createProductAndVerifyProductTest() {
+		System.out.println("The Product has been created");
+	}
+}
