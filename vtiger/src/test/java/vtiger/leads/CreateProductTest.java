6078fd46f95ee4450ec594ceb5b368d92d9ab17d
akashdeb <akashdeb87@gmail.com>
2023-05-27 16:07:52 +0530

    Verified Product with vendor

--- vtiger/src/test/java/vtiger/leads/CreateProductTest.java
+++ vtiger/src/test/java/vtiger/leads/CreateProductTest.java
@@ -8,4 +8,9 @@ public class CreateProductTest {
 	public void createProductAndVerifyProductTest() {
 		System.out.println("The Product has been created");
 	}
+	
+	@Test
+	public void createProductAndVerifyProductWithVendorDetails() {
+		System.out.println("The Product has been created and vendor deatails has been verified");
+	}
 }
